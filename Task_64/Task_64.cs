07787e560cf019a5bb7b059b17a1fbd6a083750b
Vyachaslav75 <kslava@tut.by>
2022-11-12 12:44:03 +0300

    add task 64

---
+++ Task_64/Task_64.cs
@@ -0,0 +1,23 @@
+﻿int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+
+string LineNumber(int num)
+{
+    if (num == 1) return num.ToString();
+    return num.ToString() + ", " + LineNumber(num - 1);
+}
+
+Console.Clear();
+int number = enterNumber("Введите число:  ", "Введите целое число");
+Console.WriteLine($"N={number} -> '{LineNumber(number)}'");
