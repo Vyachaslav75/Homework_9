204bb2570b5221f1057587f25274539460f3bb88
Vyachaslav75 <kslava@tut.by>
2022-11-12 12:51:21 +0300

    add task 66

---
+++ Task_66/Task_66.cs
@@ -0,0 +1,24 @@
+﻿int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+
+int SumDigits(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    return numM + SumDigits(numM + 1, numN);
+}
+
+Console.Clear();
+int numberM = enterNumber("Введите число M:  ", "Введите целое число");
+int numberN = enterNumber("Введите число N:  ", "Введите целое число");
+Console.WriteLine($"M={numberM}; N={numberN} -> {SumDigits(numberM, numberN)}");
